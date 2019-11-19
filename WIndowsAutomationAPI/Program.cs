a78917086eac8e77b4e913db97ff867043faad6c
rakesh.kumar <getrakesh.ai@gmail.com>
2019-11-19 15:12:19 +0530

    adding program.cs

--- WIndowsAutomationAPI/Program.cs
+++ WIndowsAutomationAPI/Program.cs
@@ -13,17 +13,15 @@ namespace WIndowsAutomationAPI
         static void Main(string[] args)
         {
             //Registers a method that handles UI Automation events
-            System.Windows.Automation.Automation.AddAutomationEventHandler(
-            eventId: WindowPattern.WindowOpenedEvent,
-            element: AutomationElement.RootElement,
-            scope: TreeScope.Children,
-            eventHandler: OnWindowOpened);
+            System.Windows.Automation.Automation.AddAutomationEventHandler(eventId: WindowPattern.WindowOpenedEvent,
+                element: AutomationElement.RootElement, scope: TreeScope.Children, eventHandler: OnWindowOpened);
 
 
             System.Windows.Automation.Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
 
             Console.ReadLine();
             Automation.RemoveAllEventHandlers();
+
         }
         private static void OnWindowOpened(object sender, AutomationEventArgs automationEventArgs)
         {
@@ -37,10 +35,12 @@ namespace WIndowsAutomationAPI
             {
             }
         }
+
         static AutomationElement _lastWindow;
         private static void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
         {
             AutomationElement elementFocused = sender as AutomationElement;
+
             if (elementFocused == null)
                 return;
 
@@ -54,12 +54,24 @@ namespace WIndowsAutomationAPI
                 {
                     _lastWindow = topLevelWindow;
                     Console.WriteLine("Focus moved to window: {0}", topLevelWindow.Current.Name);
+                    var child = topLevelWindow.FindAll(TreeScope.Children, Condition.TrueCondition);
+                    Console.WriteLine("...child element");
+                    foreach (var c in child)
+                    {
+                        var ch = (AutomationElement)c;
+                        Console.WriteLine(ch.Current.Name);
+                        Console.WriteLine(ch.Current.AutomationId);
+                        //Reading text from AutomationElement
+                        Console.WriteLine(AutomationExtensions.GetText(ch));
+
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Focused element: Type: '{0}', Name:'{1}'",
                     elementFocused.Current.LocalizedControlType, elementFocused.Current.Name);
                 }
+
             }
             catch (ElementNotAvailableException)
             {
@@ -85,6 +97,27 @@ namespace WIndowsAutomationAPI
             }
 
             return null;
+        }
+    }
+    public static class AutomationExtensions
+    {
+        public static string GetText(this AutomationElement element)
+        {
+            object patternObj;
+            if (element.TryGetCurrentPattern(ValuePattern.Pattern, out patternObj))
+            {
+                var valuePattern = (ValuePattern)patternObj;
+                return valuePattern.Current.Value;
+            }
+            else if (element.TryGetCurrentPattern(TextPattern.Pattern, out patternObj))
+            {
+                var textPattern = (TextPattern)patternObj;
+                return textPattern.DocumentRange.GetText(-1).TrimEnd('\r'); // often there is an extra '\r' hanging off the end.
+            }
+            else
+            {
+                return element.Current.Name;
+            }
         }
     }
 }
